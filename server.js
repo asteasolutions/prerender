f7a42ab18248b306e354c1c6f824b38fbf17ac28
Nikolay Manolov <nikolay.manolov@asteasolutions.com>
2015-11-18 18:09:26 +0200

    Configure prerender in a way that works for me

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
-var prerender = require('./lib');
-
-var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-});
-
+var prerender = require('./lib'),
+    server = prerender({
+        port: 3001,
+        workers: 4,
+        iterations: 20,
+        jsTimeout: 1000,
+        noJsExecutionTimeout: 5000
+    });
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
-// server.use(prerender.logger());
+// server.use(prerender.blacklist());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
 server.start();
